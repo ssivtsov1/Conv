23d373fc58a3302ba6c5abcd4135661183715905
ssivtsov1 <globalserg@ukr.net>
2020-04-10 20:43:40 +0300

    10.04.20_17

--- config/db_pg_dn_energo.php
+++ config/db_pg_dn_energo.php
@@ -3,7 +3,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=192.168.15.15;port=5432;dbname=energo',
+    'dsn' => 'pgsql:host=192.168.15.15;port=5432;dbname=energo_dnres',
     'username' => 'local',
     'password' => '',
     'charset' => 'utf8',
