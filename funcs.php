a299c8569eebba54ead69ea1f7aca3994fb1a014
ssivtsov1 <globalserg@ukr.net>
2020-04-15 10:05:58 +0300

    15.04.20_6

--- funcs.php
+++ funcs.php
@@ -1628,14 +1628,13 @@ else
             $town  = mb_substr($town,$pos,$y-($pos),"UTF-8") ;
             $sql = 'select town from addr_sap where (town like' . "'%" . "$town" . "%'" . ' or '. "trim(town)="."'$town'".
                 ") and trim(note)='Дніпропетровська' limit 1";
-            debug($sql);
-            return;
+
 
     }
-//        $sql = 'select town from addr_sap where (town like' . "'%" . "$$$town$$" . "%'" . ' or '. "trim(town)="."$$$town$$".
-//                     ") and trim(note)='Дніпропетровська' limit 1";
+        $sql = 'select town from addr_sap where (town like' . "'%" . "$town" . "%'" . ' or '. "trim(town)="."'$town'".
+            ") and trim(note)='Дніпропетровська' limit 1";
 
-//        debug($street_cek);
+//       debug($sql);
 //        return;
 
         $data = data_from_server($sql, (int) $rem, 1);
