14db75510c060f93b5f6f0d20e56f0ddd81d9a0c
egor <egorkonopka93@gmail.com>
2020-07-28 11:42:48 +0300

    connobj_ind_krg

--- funcs.php
+++ funcs.php
@@ -1680,7 +1680,8 @@ function f_connobj_ind($n_struct,$rem,$v) {
          else case when trim(lower(c.street))='шосе кіровоградське' then 1=1 else coalesce(lower(trim(c.type_street)),'')=coalesce(lower(trim(get_typestreet(b1.street))),'') end end 
          and trim(lower(b1.town))=trim(lower(case when c.type_city='смт.' then 'смт' else lower(c.type_city) end ||' '||trim(lower(c.town))))
          and case when trim(b1.town)='с. Інгулець' then trim(b1.rnobl)='Криворізький район' else 1=1 end 
-         and case when trim(b1.town)='с. Вільне' and $rem='05' then trim(b1.rnobl)='Криворізький район' else 1=1 end   
+         and case when trim(b1.town)='с. Вільне' and $rem='05' then trim(b1.rnobl)='Криворізький район' else 1=1 end  
+         and case when trim(b1.town)='с. Грузьке' and $rem='05' then trim(b1.rnobl)='Криворізький район' else 1=1 end    
          and case when trim(b1.town)='с. Вільне' and $rem='07' then trim(b1.rnobl)='Новомосковський район' else 1=1 end  
          and case when trim(b1.town)='с. Широке' and $rem='05' then trim(b1.rnobl)='Криворізький район' else 1=1 end  
          left join (select distinct numtown,first_value(post_index) over(partition by numtown) as post_index from  post_index_sap) b2 on b1.numtown=b2.numtown -- and b2.post_index=c.indx --and c.indx is not null
